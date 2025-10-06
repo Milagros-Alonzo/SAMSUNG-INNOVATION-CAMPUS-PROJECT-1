44afe0058c596e0bb687f1ba10dcbb52e69b07a2
admin <admin@MacBook-Air-de-admin.local>
2025-10-06 16:48:34 -0500

    ajuste en la sanatizacion de data y mapeo

--- Data_Analisis_Student/src/config.py
+++ Data_Analisis_Student/src/config.py
@@ -2,13 +2,18 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parents[1]
 
-RAW_DATA      = BASE_DIR / "data" / "raw" / "dataset_estudiantes_modifcado.csv"
-INTERIM_CSV   = BASE_DIR / "data" / "interim" / "dataset_clean.csv"
-PROCESSED_CSV = BASE_DIR / "data" / "processed" / "student_monthly_agg.csv"
+# Data
+RAW_DATA       = BASE_DIR / "data" / "raw" / "dataset_estudiantes_modifcado.csv"
+INTERIM_CSV    = BASE_DIR / "data" / "interim" / "dataset_clean.csv"
+PROCESSED_CSV  = BASE_DIR / "data" / "processed" / "student_monthly_agg.csv"
 
-MAPPING_FILE  = BASE_DIR / "docs" / "mapping_columns.json"
+# Docs / figuras
+MAPPING_FILE   = BASE_DIR / "docs" / "mapping_columns.json"
+FIG_DIR        = BASE_DIR / "docs" / "figures"
 
-# crear carpetas si no existen
+# Crear carpetas necesarias
 INTERIM_CSV.parent.mkdir(parents=True, exist_ok=True)
 PROCESSED_CSV.parent.mkdir(parents=True, exist_ok=True)
 MAPPING_FILE.parent.mkdir(parents=True, exist_ok=True)
+FIG_DIR.mkdir(parents=True, exist_ok=True)
+
